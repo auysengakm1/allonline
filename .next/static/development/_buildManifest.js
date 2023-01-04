6b1ea3fe613cc0a57bfc8131b07c484418ed5f5f
unknown <auysengakm1@gmail.com>
2023-01-04 20:49:09 +0700

    first commit

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/flashsale":["static\u002Fchunks\u002Fpages\u002Fflashsale.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fflashsale"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
